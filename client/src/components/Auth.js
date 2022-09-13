effcc2f8e233e2107848fc7d0bdcb7e65bae5594
joagaloppo <joaquingaloppo@gmail.com>
2022-09-13 10:36:53 -0300

    Implement auth in client side

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Auth() {
+	const navigate = useNavigate();
 	const [input, setInput] = React.useState({username: "", password: ""});
     const [login, setLogin] = React.useState(true);
 
+	React.useEffect(() => {
+		axios
+			.get(process.env.REACT_APP_GET_USER_AUTH, { withCredentials: true })
+			.then((res) => {
+				if (res.data) navigate('/dashboard');
+			})
+			.catch((err) => console.log(err));
+	}, [navigate]);
+
 	const handleChange = (e) => { setInput({ ...input, [e.target.name]: e.target.value, }); };
 
-	const handleSubmit = (e) => { e.preventDefault(); };
+	const handleSubmit = (e) => { 
+		e.preventDefault();
+
+		axios
+			.post(e.target.name === "login"
+            ? process.env.REACT_APP_POST_USER_LOGIN
+            : process.env.REACT_APP_POST_USER_REGISTER,
+            input, { withCredentials: true })
+			.then(() => { login ? navigate("/dashboard") : setLogin(true); setInput({username: "", password: ""});})
+			.catch((err) => console.log(err.response.data.message));
+	};
 
 	return (
 		<div className='form auth'>
