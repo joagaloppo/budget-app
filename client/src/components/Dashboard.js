91c7d2c77b1dffa50a1d3f4581ecfd5fd7fe19c7
joagaloppo <joaquingaloppo@gmail.com>
2022-09-13 10:37:08 -0300

    Prevent interaction without being logged

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 import Nav from './Nav.js';
 
 function Dashboard() {
+    const navigate = useNavigate();
+    const [user, setUser] = React.useState();
+    
+    const fetchData = React.useCallback(() => {
+		const isValidated = async () => {
+			const res = await axios.get(process.env.REACT_APP_GET_USER_AUTH, { withCredentials: true });
+			if (!res.data) return navigate('/');
+			setUser(res.data);
+			return res.data;
+		};
+
+		isValidated();
+	}, [navigate]);
+
+    React.useEffect(() => {
+		fetchData();
+	}, [fetchData]);
+
 	return (
 		<React.Fragment>
             <Nav />
