81a17c2b717364428c4844629c0c9b86a1963655
joagaloppo <joaquingaloppo@gmail.com>
2022-09-13 14:33:38 -0300

    Add logout function to nav component

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function Nav( { user } ) {
+
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        axios.post(process.env.REACT_APP_POST_USER_LOGOUT, null, { withCredentials: true })
+            .then(() => navigate("/") )
+            .catch(err => console.log(err.response.data) )
+    }
+
     return (
 		<div className='nav'>
             <div className='container'>
                 <h1>Budget App</h1>
-                <span>{user.email}</span>
+                <span>{user.email}<i href="#" onClick={() => handleLogOut()} className="logout">Logout</i></span>
             </div>
         </div>
 	);
