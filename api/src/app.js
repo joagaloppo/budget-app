0331f31691e54ffbbfb4b27159d2b2983385d1f2
joagaloppo <joaquingaloppo@gmail.com>
2022-09-13 10:21:22 -0300

    Initial passport setup

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,31 @@
 import express from 'express';
+const app = express();
+
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+
 import authRoutes from './routes/auth.routes.js'
 import transactionRoutes from './routes/transaction.routes.js'
 
-const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: process.env.CLIENT_FULL_URL,
+    credentials: true,
+}));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/transaction", transactionRoutes);
 
--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,9 +1,61 @@
 import { User } from '../models/User.js';
 
-export const login = async (req, res) => {}
+import LocalStrategy from 'passport-local'
+import passport from 'passport';
+import bcrypt from 'bcrypt';
 
-export const register = async (req, res) => {}
+passport.use(new LocalStrategy((email, password, done) => {
+    User.findOne({ where: { email } })
+    .then(user => {
+        if (!user) return done(null, false, { message: `Email doesn't exist` })
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (res) return done(null, user);
+            return done(null, false, { message: 'Password is incorrect' });
+        });
+    });
+}));
 
-export const logout = async (req, res) => {}
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
 
-export const auth = async (req, res) => {}
+passport.deserializeUser((id, done) => {
+    User.findByPk(id).then(user => done(null, user))
+});
+
+export const login = async (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return next(err)
+        if (!user) return res.status(401).json(info)
+        req.logIn(user, (err) => {
+            if (err) return next(err)
+            return res.send(user)
+        });
+    })(req, res, next)
+}
+
+export const register = async (req, res, next) => {
+    User.findOne({ where: { email: req.body.username }})
+    .then(user => {
+        if (user) return res.status(409).json({ message: 'Email already exists' })
+        bcrypt.hash(req.body.password.trim(), 10, (err, hash) => {
+            if (err) return next(err)
+            User.create({
+                email: req.body.username.trim(),
+                password: hash
+            }).then(user => res.send(user))
+        })
+    })
+    .catch(err => console.log(err))
+}
+
+export const logout = async (req, res, next) => {
+    req.logout(err => {
+        if (err) return next(err)
+        res.send({ message: 'Logged out' })
+    });
+}
+
+export const auth = async (req, res) => {
+    res.json(req.user);
+}
