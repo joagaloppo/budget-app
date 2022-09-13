1bade2cdc5bf1921d7752c9105189d51d6cbf5e1
joagaloppo <joaquingaloppo@gmail.com>
2022-09-13 09:49:33 -0300

    Add routes for each component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+
+// Components
+import Dashboard from "./components/Dashboard.js";
+import Auth from "./components/Auth.js";
 
 function App(props) {
   return (
-    <React.Fragment> Hello World </React.Fragment>
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Auth/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
