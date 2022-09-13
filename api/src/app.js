cea25c292ee34fc4a0da15c0518115408ccad04e
joagaloppo <joaquingaloppo@gmail.com>
2022-09-13 08:41:46 -0300

    Add routes to app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import authRoutes from './routes/auth.routes.js'
+import transactionRoutes from './routes/transaction.routes.js'
 
 const app = express();
+
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/transaction", transactionRoutes);
 
 export default app;
