49d5cfb334d75c807bdf7d7970e25effed98d3b6
joagaloppo <joaquingaloppo@gmail.com>
2022-09-14 09:33:06 -0300

    Add validation to forms

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function Auth() {
 	const navigate = useNavigate();
 	const [input, setInput] = React.useState({username: "", password: ""});
+	const [error, setError] = React.useState({});
     const [login, setLogin] = React.useState(true);
 
 	React.useEffect(() => {
@@ -18,33 +19,56 @@ function Auth() {
 
 	const handleChange = (e) => { setInput({ ...input, [e.target.name]: e.target.value, }); };
 
-	const handleSubmit = (e) => { 
+	const handleSubmit = (e) => {
 		e.preventDefault();
-
+        const err = handleValidation();
+        if (err.username || err.password ) return setError(err);
 		axios
 			.post(e.target.name === "login"
             ? process.env.REACT_APP_POST_USER_LOGIN
             : process.env.REACT_APP_POST_USER_REGISTER,
             input, { withCredentials: true })
-			.then(() => { login ? navigate("/dashboard") : setLogin(true); setInput({username: "", password: ""});})
-			.catch((err) => console.log(err.response.data.message));
+			.then(() => { login ? navigate("/dashboard") : setLogin(true); setError({}); setInput({username: "", password: ""});})
+			.catch((err) => setError({database: err.response.data.message}));
 	};
 
-	return (
-		<div className='form auth'>
-			<div className='box auth'>
-				<h2> { login ? "Login" : "Register" } </h2>
-				<form>
-					<div>
-						<input type="email" placeholder="Email" name="username" value={input.username} onChange={(e) => {handleChange(e); }}/>
-					</div>
-					<div>
-						<input type="password" placeholder="Password" name="password" value={input.password} onChange={(e) => {handleChange(e); }}/>
-					</div>
-					<button name={login ? "login" : "register"} onClick={(e) => handleSubmit(e)}> { login ? "Login" : "Register" } </button>
-				</form>
-				<span onClick={() => {setLogin(!login); setInput({username: "", password: ""}); }}>{ login ? "Don't have an account?" : "Already have an account?" }</span>
-			</div>
+    const handleValidation = (e) => {
+        const err = {username:"", password: ""};
+        if (!login) {
+            if (!input.username) err.username = "Email is missing"
+            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(input.username)) err.username = "Email is not valid"
+            else err.username = "";
+            if (!input.password) err.password = "Password is missing"
+            else if (input.password.length < 6) err.password = "Password is too short"
+            else err.password = "";
+        } else {
+            if (!input.username) err.username = "Email is missing"
+            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(input.username)) err.username = "Enter a valid email" 
+            else err.username = "";
+            if (!input.password) err.password = "Password is missing"
+            else err.password = "";
+        }
+        return err;
+    }
+
+    return (
+    <div className='form auth'>
+        <div className='box auth'>
+            <h2> { login ? "Login" : "Register" } </h2>
+            <form>
+                {error.database ? <span className='error'>{error.database}</span> : <span className="space"/>}
+                <div className={error.username ? "input error" : null}>
+                    <input type="email" placeholder="Email" name="username" value={input.username} onChange={(e) => {handleChange(e); setError({...error, username:""})}}/>
+                    {error.username ? <span className='error'>{error.username}</span> : <span className="space"/>}
+                </div>
+                <div className={error.password ? "input error" : null}>
+                    <input type="password" placeholder="Password" name="password" value={input.password} onChange={(e) => {handleChange(e); setError({...error, password:""})}}/>
+                    {error.password ? <span className='error'>{error.password}</span> : <span className="space"/>}
+                </div>
+                <button name={login ? "login" : "register"} onClick={(e) => handleSubmit(e)}> { login ? "Login" : "Register" } </button>
+            </form>
+            <span onClick={() => {setLogin(!login); setInput({username: "", password: ""}); setError({})}}>{ login ? "Don't have an account?" : "Already have an account?" }</span>
+        </div>
     </div>
     );
 }
--- client/src/components/Manage.js
+++ client/src/components/Manage.js
@@ -8,22 +8,29 @@ function Manage( { user, transactions, balance, fetchData } ) {
     const [display, setDisplay] = React.useState("");
     const [type, setType] = React.useState(true);
     const [input, setInput] = React.useState({ detail: "", amount: 0, date: "", type: "" });
+    const [error, setError] = React.useState({ detail: '', amount: '', date: '', type: '' })
 
     const handleChange = (e) => { setInput ({ ...input, [e.target.name]: e.target.value }) }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const err = handleValidation();
+        if (err.detail || err.amount || err.date || err.type ) return setError(err);
         axios.post(process.env.REACT_APP_POST_TRANSACTION, { ...input, userId: user.id })
             .then(res => { setDisplay(""); fetchData(); setInput({ detail: "", amount: 0, date: "", type: "" });})
             .catch(err => console.log(err.response.data))
+
         setInput({ detail: "", amount: 0, date: "", type: "" });
     }
 
     const handleEdit = (e) => {
         e.preventDefault();
+        const err = handleValidation();
+        if (err.detail || err.amount || err.date) return setError(err);
         axios.put(`${process.env.REACT_APP_POST_TRANSACTION}/${input.id}`, { detail: input.detail, amount: input.amount, date: input.date })
             .then(res => { setDisplay(""); fetchData(); setInput({ detail: "", amount: 0, date: "", type: "" });})
             .catch(err => console.log(err.response.data))
+
     }
 
     const handleDelete = (e) => {
@@ -36,6 +43,22 @@ function Manage( { user, transactions, balance, fetchData } ) {
         const t = transactions.find(a => a.id === parseInt(e.target.name));
         setInput({ id: t.id, detail: t.detail, amount: t.amount, date: t.date })
         setDisplay("edit");
+    }
+
+    const handleValidation = (e) => {
+        const err = { detail: '', amount: '', date: '', type: '' };
+        if (!input.detail) err.detail = "Detail is missing";
+        else if (input.detail.length < 2) err.detail = "Detail is too short";
+        else err.detail = "";
+        if (input.amount <= 0) err.amount = `Amount can't be negative`;
+        else err.amount = "";
+        if (!input.date) err.date = `Date can't be null`;
+        else err.date = "";
+        if (display === "add") {
+            if (!input.type) err.type = `Type can't be null`;
+            else err.type = "";
+        }
+        return err;
     }
 
     return (
@@ -89,35 +112,39 @@ function Manage( { user, transactions, balance, fetchData } ) {
             <div className="box manage">
                 <form>
                     <div className="input-section">
-                        <div>
+                        <div className={error.detail ? "input error" : null}>
                             <div className="join">
-                                <label>Detail</label> <input type="text" name="detail" onChange={(e) => {handleChange(e); }} value={input.detail}/>
+                                <label>Detail</label> <input type="text" name="detail" onChange={(e) => {handleChange(e); setError({...error, detail:""})}} value={input.detail}/>
                             </div>
+                            {error.detail ? <span className='error'>{error.detail}</span> : null}
                         </div>
-                        <div>
+                        <div className={error.amount ? "input error" : null}>
                                 <div className="join">
-                            <label>Amount</label> <input type="number" name="amount" onChange={(e) => {handleChange(e); }} value={input.amount} className="amount"/>
+                            <label>Amount</label> <input type="number" name="amount" onChange={(e) => {handleChange(e); setError({...error, amount:""})}} value={input.amount} className="amount"/>
                                 </div>
+                            {error.amount ? <span className='error'>{error.amount}</span> : null}
                         </div>
-                        <div>
+                        <div className={error.date ? "input error" : null}>
                             <div className="join">
-                                <label>Date</label> <input type="date" name="date" onChange={(e) => {handleChange(e); }} value={input.date} className="date"/>
+                                <label>Date</label> <input type="date" name="date" onChange={(e) => {handleChange(e); setError({...error, date:""})}} value={input.date} className="date"/>
                             </div>
+                            {error.date ? <span className='error'>{error.date}</span> : null}
                         </div>
                         { display === "edit" ? null : (
-                            <div>
+                            <div className={error.type ? "input error" : null}>
                                 <div className="join">
                                     <label>Type</label>
                                     <div className="buttons">
-                                        <button className={input.type === "in" ? "in active" : "in"} onClick={(e) => { e.preventDefault(); setInput ({ ...input, type: "in" }); }}> In </button>
-                                        <button className={input.type === "out" ? "out active" : "out"} onClick={(e) => { e.preventDefault(); setInput ({ ...input, type: "out" }); }}> Out </button>
+                                        <button className={input.type === "in" ? "in active" : "in"} onClick={(e) => { e.preventDefault(); setInput ({ ...input, type: "in" }); setError({...error, type:""})}}> In </button>
+                                        <button className={input.type === "out" ? "out active" : "out"} onClick={(e) => { e.preventDefault(); setInput ({ ...input, type: "out" }); setError({...error, type:""})}}> Out </button>
                                     </div>
                                 </div>
+                                {error.type ? <span className='error'>{error.type}</span> : null}
                             </div>
                         )}
                     </div>
                     <div className='button-section'>
-                        <button onClick={(e) => { e.preventDefault(); setDisplay(""); setInput({ detail: "", amount: 0, date: "", type: "" } ) } }>Cancel</button>
+                        <button onClick={(e) => { e.preventDefault(); setDisplay(""); setError({ detail: '', amount: '', date: '', type: '' }); setInput({ detail: "", amount: 0, date: "", type: "" } ) } }>Cancel</button>
                         <button onClick={(e) => display === "add" ? handleSubmit(e) : handleEdit(e) }> { display === "add" ? "Create" : "Edit" }</button>
                     </div>
                 </form>
