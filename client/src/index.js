491c7b1de85b7accb00e8ecc162c4643f13125b6
joagaloppo <joaquingaloppo@gmail.com>
2022-09-14 14:10:55 -0300

    Add styling

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+// Components
 import App from './App.js';
 
+// Styles
 import './styles/index.css';
+import './styles/auth.css';
+import './styles/nav.css';
+import './styles/summary.css';
 import './styles/manage.css';
+import './styles/form.css';
+import './styles/responsive.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
